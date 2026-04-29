c4c2288e12427b8db4add17defb6d61c029e057d
DhananjaiPS <mahakkr111@gmail.com>
2026-04-30 03:03:36 +0530

    ui login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      email: 'r@gmail.com',
+      email: 'Admin@gmail.com',
       password: '123456'
     }
   });
